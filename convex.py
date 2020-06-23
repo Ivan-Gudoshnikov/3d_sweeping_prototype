linprog option; force sign issue fixed

convex.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import numpy as np
+import scipy.optimize
 
 from quadprog_interface import QuadprogInterface, McGibbonQuadprog
 
@@ -82,8 +83,14 @@ class Polytope(ConvexSet):
         # 1/2 proj^T.H.proj + (- H.x)^T.proj
         #  1/2 x^T.H.x + c^T.x
 
-        x = method.quadprog(H, -np.matmul(H,x), self.A, self.b, self.Aeq, self.beq)
+        x, active = method.quadprog(H, -np.matmul(H,x), self.A, self.b, self.Aeq, self.beq)
         return x
+
+    def linprog(self, c):
+        result = scipy.optimize.linprog(c, self.A, self.b, self.Aeq, self.beq)
+        if not result[2]:
+            raise NameError("Can't do linprog, errorcode", result[6])
+        return result[0]
 
     def __contains__(self, x):
         if x.shape[0]!=self.n:
@@ -165,47 +172,54 @@ def test_polytope_projection():
     poly1=Polytope(A,b,Aeq,beq)
 
     H1=np.identity(3)
-    x1=np.array([2, 0.1, -0.1])
-    print(poly1.projection(H1,x1,McGibbonQuadprog()))
+    x=np.array([2, 0.1, -0.1])
+    p,a = poly1.projection(H1,x,McGibbonQuadprog())
+    print(p,a)
 
-    x2=np.array([0.1, 10, 3])
-    print(poly1.projection(H1, x2, McGibbonQuadprog()))
+    x=np.array([0.1, 10, 3])
+    p, a = poly1.projection(H1, x, McGibbonQuadprog())
+    print(p, a)
 
-    x3=np.array([-0.2, -0.1, 3])
-    print(poly1.projection(H1, x3, McGibbonQuadprog()))
+    x=np.array([-0.2, -0.1, 3])
+    p, a = poly1.projection(H1, x, McGibbonQuadprog())
+    print(p, a)
 
-    x4 = np.array([-10, -10, -10])
-    print(poly1.projection(H1, x4, McGibbonQuadprog()))
+    x = np.array([-10, -10, -10])
+    p, a = poly1.projection(H1, x, McGibbonQuadprog())
+    print(p, a)
+
 
     print("--------")
-    box1=Box(bmin=np.array([-5.,-5.]), bmax=np.array([1.,2.]))
+    box1 = Box(bmin=np.array([-5.,-5.]), bmax=np.array([1.,2.]))
     H2=np.diag([1.,1.])
 
-    x1=np.array([2,3])
-    p1=box1.projection(H2, x1, McGibbonQuadprog())
-    print(p1)
-    print(box1.normal_cone(H2,p1).N)
+    x=np.array([2,3])
+    p, a=box1.projection(H2, x, McGibbonQuadprog())
+    print(p, a)
+    print(box1.normal_cone(H2,p).N)
 
-    x2=np.array([-6,-10])
-    p2 = box1.projection(H2, x2, McGibbonQuadprog())
-    print(p2)
-    print(box1.normal_cone(H2, p2).N)
-
-    x3=np.array([-6,7])
-    p3 = box1.projection(H2, x3, McGibbonQuadprog())
-    print(p3)
-    print(box1.normal_cone(H2, p3).N)
+    x=np.array([-6,-10])
+    p, a = box1.projection(H2, x, McGibbonQuadprog())
+    print(p, a)
+    print(box1.normal_cone(H2, p).N)
 
 
-    x4=np.array([0.1,0.1])
-    p4 = box1.projection(H2, x4, McGibbonQuadprog())
-    print(p4)
-    print(box1.normal_cone(H2, p4).N)
+    x=np.array([-6,7])
+    p, a = box1.projection(H2, x, McGibbonQuadprog())
+    print(p, a)
+    print(box1.normal_cone(H2, p).N)
 
-    x5=np.array([10,0.21])
-    p5 = box1.projection(H2, x5, McGibbonQuadprog())
-    print(p5)
-    print(box1.normal_cone(H2, p5).N)
+
+    x=np.array([0.1,0.1])
+    p, a = box1.projection(H2, x, McGibbonQuadprog())
+    print(p, a)
+    print(box1.normal_cone(H2, p).N)
+
+
+    x=np.array([10,0.21])
+    p, a = box1.projection(H2, x, McGibbonQuadprog())
+    print(p, a)
+    print(box1.normal_cone(H2, p).N)
 
 
 if __name__ == "__main__":

elastoplastic_process.py
@@ -275,7 +275,7 @@ class ElastoplasticProcess:
         #beq = - np.matmul(Aeq, np.matmul(self.u_basis(xi, t), self.h_u_coords(xi, t)))
 
         Aeq = self.p_u_coords(xi, t)
-        beq = -self.h_u_coords(xi, t, self.f(t))
+        beq = self.h_u_coords(xi, t, self.f(t))
 
         return Polytope(A, b, Aeq, beq)
 
@@ -299,12 +299,16 @@ class ElastoplasticProcess:
             A = np.hstack((np.zeros((l_size,dim_u)), - np.identity(l_size)))
             b = np.zeros(l_size)
 
-            #TODO: alternative ways to find \dot xi from its constraints?
-            u_and_l = McGibbonQuadprog().quadprog(np.identity(dim_u+l_size), np.zeros(dim_u+l_size), A, b, Aeq, beq)
-            u_coords = u_and_l[range(0, dim_u)]
-            dot_p = np.matmul(N, u_and_l[range(dim_u,dim_u+l_size)])
+            #TODO: alternative ways to find \dot xi from its constraints?\
+            param_set = Polytope(A,b, Aeq,beq)
+
+            u_and_l_params = param_set.projection(np.identity(dim_u+l_size), np.zeros(dim_u+l_size), McGibbonQuadprog())
+            #u_and_l_params = param_set.linprog(np.hstack((np.zeros(dim_u), np.ones(l_size)))) #linprog is actually more strict
+
+            u_coords = u_and_l_params[range(0, dim_u)]
+            dot_p = np.matmul(N, u_and_l_params[range(dim_u,dim_u+l_size)])
         else:
-            #if dot p = 0 we have  - dot x = - dot e in U + np.matmul(np.matmul(self.d_xi_phi(xi), self.R(xi,t)), self.d_t_rho(xi,t)) du to sweeping process
+            #if dot p = 0 we have  - dot x = - dot e in U + np.matmul(np.matmul(self.d_xi_phi(xi), self.R(xi,t)), self.d_t_rho(xi,t)) due to sweeping process
             # take projection to fund u-coords
             u_coords = np.matmul(self.p_u_coords(xi, t), dot_e + np.matmul(np.matmul(self.d_xi_phi(xi), self.R(xi,t)), self.d_t_rho(xi,t)))
             dot_p = np.zeros(self.m)

quadprog_interface.py
@@ -76,5 +76,10 @@ class McGibbonQuadprog(QuadprogInterface):
             b_qp = - np.hstack((beq, b))
             meq_qp = Aeq.shape[0]
 
-        result_qp = mcgibbon_quadprog.solve_qp(G_qp, a_qp, C_qp, b_qp, meq_qp)
-        return result_qp[0]
+        (x_qp, f_qp, xu_qp, iter_qp, lag_qp, iact_qp) = mcgibbon_quadprog.solve_qp(G_qp, a_qp, C_qp, b_qp, meq_qp)
+
+        #active = iact_qp[iact_qp >= meq_qp]-meq_qp
+        if 0 in iact_qp:
+            raise NameError("kukuku")
+
+        return x_qp, iact_qp